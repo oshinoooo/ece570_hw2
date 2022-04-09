ecff2e36bbb9c77f987c7e3835beb0c7184d660d
oshinoooo <1036858975@qq.com>
2022-04-09 14:21:55 -0700

    2022-04-09 14:21:55

---
+++ src/pager.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+#include "vm_app.h"
+#include "vm_pager.h"
+
+using namespace std;
+
+int main() {
+
+
+
+    return 0;
+}
---
+++ tests/test1.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+#include "../src/vm_app.h"
+#include "../src/vm_pager.h"
+
+using namespace std;
+
+int main() {
+
+
+
+    return 0;
+}
