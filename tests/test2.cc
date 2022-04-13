cc65749589c00118af6c397ed2b2a850476a5da9
oshinoooo <1036858975@qq.com>
2022-04-13 08:10:45 -0700

    2022-04-13 08:10:45

--- tests/test2.cc
+++ tests/test2.cc
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <vector>
 
-#include "vm_app.h"
-#include "vm_pager.h"
+#include "../src/vm_app.h"
+#include "../src/vm_pager.h"
 
 using namespace std;
 
 int main() {
-    cout << "Hello World!" << endl;
+
+
+
     return 0;
 }
