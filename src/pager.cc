4f5163c70dc0c0cdd93b16e53b00c035561119cc
oshinoooo <1036858975@qq.com>
2022-04-16 08:32:29 -0700

    2022-04-16 08:32:29

--- src/pager.cc
+++ src/pager.cc
@@ -6,6 +6,9 @@
 using namespace std;
 
 int main() {
+    cout << "------------" << endl;
+    cout << VM_ARENA_SIZE << endl;
     cout << "Hello World!" << endl;
+    cout << "------------" << endl;
     return 0;
 }
--- tests/test1.cc
+++ tests/test1.cc
@@ -6,8 +6,12 @@
 using namespace std;
 
 int main() {
-
-
-
+    char* p = (char*)vm_extend();
+    p[0] = 'h';
+    p[1] = 'e';
+    p[2] = 'l';
+    p[3] = 'l';
+    p[4] = 'o';
+    vm_syslog(p, 5);
     return 0;
 }
