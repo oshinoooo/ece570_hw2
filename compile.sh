849dc812a7d6659c86f587e3e1a95634d3c89201
oshinoooo <1036858975@qq.com>
2022-04-17 14:57:03 -0700

    2022-04-17 14:57:03

--- compile.sh
+++ compile.sh
@@ -2,8 +2,5 @@
 
 #submit570 2 ./src/pager.cc ./tests/test1.cc ./tests/test2.cc
 
-rm pager
 g++ -m32 -I./src ./src/pager.cc lib/libvm_pager.a -o pager
-
-rm test
 g++ -m32 -I./tests ./tests/test1.cc lib/libvm_app.a -o test
